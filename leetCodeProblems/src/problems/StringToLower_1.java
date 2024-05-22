52ef30e4e4eac604c096323a6900e197a218bbb2
Seyed Muzafar <iamMseyed@users.noreply.github.com>
2024-05-22 20:37:02 +0530

    StringToLower_1.java

--- leetCodeProblems/src/problems/StringToLower_1.java
+++ leetCodeProblems/src/problems/StringToLower_1.java
@@ -1,11 +1,7 @@
 package problems;
-
 import java.util.Scanner;
-
 public class StringToLower_1 {
-
 	public static void main(String[] args) {
-		
 		Scanner s = new Scanner(System.in);
 		System.out.println("Enter any String value");
 		String orgStr = s.nextLine();
